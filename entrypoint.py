950992233a5fa20a5ace62fa9e467613077987ba
Jacek Wielemborek <d33tah@gmail.com>
2020-04-26 14:53:03 +0200

    Rewrite the code to asyncio.

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2
 
-import socket
-import ssl
+import asyncio
 import struct
 
 import Mumble_pb2 as pb2
@@ -23,57 +22,45 @@ messageTypes = {
     13: pb2.ACL,
     14: pb2.QueryUsers,
     15: pb2.CryptSetup,
+    16: pb2.ContextActionModify,
+    17: pb2.ContextAction,
+    18: pb2.UserList,
+    19: pb2.VoiceTarget,
+    20: pb2.PermissionQuery,
+    21: pb2.CodecVersion,
+    22: pb2.UserStats,
+    23: pb2.RequestBlob,
+    24: pb2.ServerConfig,
+    25: pb2.SuggestConfig,
 }
 
 for k, v in messageTypes.items():
     v.typeID = k
 
 
-def openConnection(hostname, port):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((hostname, port))
-        return ssl.wrap_socket(s)
-    except ssl.SSLError:
-        s.close()
-
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((hostname, port))
-        return ssl.wrap_socket(s, ssl_version=ssl.PROTOCOL_TLSv1)
-    except ssl.SSLError:
-        s.close()
-        raise Exception("Error setting up the SSL/TLS socket to murmur.")
-
-
-def send_message(message, conn):
+async def send_message(message, conn):
     stringMessage = message.SerializeToString()
     length = len(stringMessage)
     header = struct.pack(">HI", message.typeID, length)
-    packedMessage = header + stringMessage
-    while len(packedMessage) > 0:
-        sent = conn.send(packedMessage)
-        if sent < 0:
-            raise Exception("could not send message")
-        packedMessage = packedMessage[sent:]
-    return True
+    conn.write(header + stringMessage)
+    await conn.drain()
 
 
-def initConnection(conn, nickname):
+async def initConnection(conn, nickname):
     msg = pb2.Version()
     msg.release = "1.2.6"
     msg.version = 0x010206
     msg.os = 'Linux'
     msg.os_version = "mumblebot lol"
-    send_message(msg, conn)
+    await send_message(msg, conn)
     msg = pb2.Authenticate()
     msg.username = nickname
     msg.opus = True
-    send_message(msg, conn)
+    await send_message(msg, conn)
 
 
-def listen(conn):
-    header = conn.recv(6)
+async def listen(conn):
+    header = await conn.read(6)
     if len(header) == 6:
         (mid, size) = struct.unpack(">HI", header)
     else:
@@ -81,9 +68,10 @@ def listen(conn):
 
     data = bytearray()
     while len(data) < size:
-        data.extend(conn.recv(size - len(data)))
+        data.extend(await conn.read(size - len(data)))
 
     if mid not in messageTypes:
+        return
         raise Exception('Unexpected message type: %r' % mid)
     messagetype = messageTypes[mid]
     msg = messagetype()
@@ -94,15 +82,17 @@ def listen(conn):
     return msg
 
 
-def main():
+async def main():
 
-    conn = openConnection('junkcc.net', 64738)
-    initConnection(conn, 'test')
+    conn_r, conn_w = await asyncio.open_connection(
+        'junkcc.net', 64738, ssl=True
+    )
+    await initConnection(conn_w, 'test')
 
     while True:
-        msg = listen(conn)
+        msg = await listen(conn_r)
         print(msg)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
