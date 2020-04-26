0086c0fc8696f340be8194dfdad1da327b53a7fb
Jacek Wielemborek <d33tah@gmail.com>
2020-04-26 17:26:15 +0200

    Add bridge.py stub.

---
+++ bridge.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import asyncio
+import mumble
+import irc
+import logging
+
+
+async def main():
+    pass
+
+if __name__ == '__main__':
+    logging.basicConfig(level='DEBUG')
+    asyncio.run(main)
